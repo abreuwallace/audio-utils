02ab32c55710b6678bf7aef151cbf8052548eb43
abreuwallace <abreu.encgb@poli.ufrj.br>
2023-10-21 20:20:40 -0300

    update

--- src/signal_processing.py
+++ src/signal_processing.py
@@ -8,19 +8,20 @@ def to_mono(x):
     
 def overlap_and_add(chunks, overlap=256, window_len=1024):
     W = window_len
-    win_left_side = np.hanning(2 * overlap)[:overlap]
-    win_right_side = np.hanning(2 * overlap)[overlap:]
+    win_left_side = np.bartlett(2 * overlap)[:overlap]
+    win_right_side = np.bartlett(2 * overlap)[overlap:]
     window = np.concatenate((win_left_side, np.ones(W - 2 * overlap), win_right_side))
     left_window = np.concatenate((np.ones(W - overlap), win_right_side))
     right_window = np.concatenate((win_left_side, np.ones(W - overlap)))    
     n_chunks = len(chunks)
     for i in range(n_chunks):
         if i == 0:
-            y = (chunks[i] * left_window).reshape(-1,)
+            y = (chunks[i].reshape(-1,) * left_window)
         else:
             x_chunk = chunks[i].reshape(-1,)
-            if len(x_chunk) < W:
-                x_chunk = np.pad(x_chunk, (0, W - len(x_chunk)), 'constant', constant_values=0)
+            if len(x_chunk) < W or i == n_chunks - 1:
+                end_pad = W - len(x_chunk)
+                x_chunk = np.pad(x_chunk, (0, end_pad), 'constant', constant_values=0)
                 x_ola = x_chunk * right_window
             else:
                 x_ola = x_chunk * window
